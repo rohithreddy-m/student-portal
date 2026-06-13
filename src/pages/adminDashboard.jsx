55bc1de16a2d6794251d7b30c9b08dcace4df2d7
rohith <mallidisairohithreddy@gmail.com>
2026-06-13 12:21:42 +0530

    all is done

--- src/pages/adminDashboard.jsx
+++ src/pages/adminDashboard.jsx
@@ -1,6 +1,66 @@
+import { useState } from "react"
+
+const studentData = {
+  name: "Rohith",
+  age: 19,
+  grade: 10,
+  targetScore: 90,
+  averageScore: 82,
+  progress: "17%",
+  latestTestDate: "2026-05-20"
+}
+const scores = [
+  {
+    subject: "Math",
+    score: 91
+  },
+  {
+    subject: "Reading",
+    score: 80
+  },
+  {
+    subject: "Writing",
+    score: 76
+  },
+  {
+    subject: "Science",
+    score: 88
+  },
+  {
+    subject: "English",
+    score: 84
+  }
+]
+
 function AdminDashboard(){
+       const [search,setSearch]=useState("")
+       function answer(){
+            def
+        if (search===studentData.name){
+            console.log("True")
+        }
+        else {
+            console.log("falae")
+        }
+       }
     return(
-        "AdminDashboard"
+        <>
+        <h1>AdminDashboard</h1>
+        <div>
+            <form onSubmit={answer}>
+                <label>Search:
+                <input
+                placeholder="Student Name"
+                value={search}
+                onChange={(e)=>setSearch(e.target.value)}
+                />
+                </label>
+
+                <button type="submit"> submit </button>
+            </form>
+        </div>
+        <h3>{search}</h3>
+        </>
     )
 }
 export default AdminDashboard
--- src/pages/studentDashboard.jsx
+++ src/pages/studentDashboard.jsx
@@ -1,6 +1,59 @@
+import "./studentDashboard.css"
 function StudentDashboard(){
+const studentData = {
+  name: "Rohith",
+  age: 19,
+  grade: 10,
+  targetScore: 90,
+  averageScore: 82,
+  progress: "17%",
+  latestTestDate: "2026-05-20"
+}
+const scores = [
+  {
+    subject: "Math",
+    score: 91
+  },
+  {
+    subject: "Reading",
+    score: 80
+  },
+  {
+    subject: "Writing",
+    score: 76
+  },
+  {
+    subject: "Science",
+    score: 88
+  },
+  {
+    subject: "English",
+    score: 84
+  }
+]
     return(
-        "StudentDashboard"
+      <>
+      <h1>StudentDashboard</h1>
+      <div className="studentpage">
+        <div className="studentinfo">
+          <h2>Student Information</h2>
+           <h5>Name:{studentData.name}</h5>
+           <h5>Age:{studentData.age}</h5>
+           <h5>Grade:{studentData.grade}</h5>
+           <h5>Target Score:{studentData.targetScore}</h5>
+          <h2>Progress Summary</h2>
+           <h5>Average Score:{studentData.averageScore}</h5>
+           <h5>Progress:{studentData.progress}</h5>
+           <h5 className="LatestTestDate">Latest Test Date:{studentData.latestTestDate}</h5>
+        </div>
+        <div className="studentScore">
+          <h2>Score</h2>
+            {scores.map((items) =>
+            <h5 className={items.subject} key={items.subject}>{items.subject}:{items.score}</h5>
+          )}
+        </div>
+        </div>
+          </>
     )
 }
 export default StudentDashboard
