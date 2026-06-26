9a03ecdb840891cd4ac0439fb5f8795db70a6745
rohith <mallidisairohithreddy@gmail.com>
2026-06-26 13:23:27 +0530

    AA

--- src/pages/adminDashboard.jsx
+++ src/pages/adminDashboard.jsx
@@ -33,34 +33,99 @@ const scores = [
 ]
 
 function AdminDashboard(){
-       const [search,setSearch]=useState("")
-       function answer(){
-            def
-        if (search===studentData.name){
-            console.log("True")
+    const [search,setSearch]=useState("")
+    const [datafound,setdatafound]=useState(false)
+    const [show,setshow]=useState(false)
+    const [edit,setedit]=useState(false)
+    const [subject,setsubject]=useState("")
+    const [score,setscore]=useState("")
+    const [scoressta,setscores]=useState([
+                                {
+                                  subject: "Math",
+                                  score: 91
+                                },
+                                {
+                                  subject: "Reading",
+                                  score: 80
+                                },
+                                {
+                                  subject: "Writing",
+                                  score: 76
+                                },
+                                {
+                                  subject: "Science",
+                                  score: 88
+                                },
+                                {
+                                  subject: "English",
+                                  score: 84
+                                }
+                              ])
+    function Edit(){
+      setedit(true)
+    }
+    const UPdate= (e)=>{
+      e.preventDefault()
+      scoressta.forEach((items)=>  
+      {if (items.subject.toLowerCase().trim()===subject.toLowerCase().trim()){
+          setscores(...scoressta,items.score=score)
+          console.log(scoressta)
+      } else {
+          console.log()
+        }})
         }
-        else {
-            console.log("falae")
+        function answer(e){
+          e.preventDefault()
+          setshow(true)
+          if (search.toLowerCase().trim()===studentData.name.toLowerCase().trim()){
+            setdatafound(true)
+          }
+          else {
+            setdatafound(false)
+          }
         }
-       }
     return(
-        <>
-        <h1>AdminDashboard</h1>
-        <div>
-            <form onSubmit={answer}>
-                <label>Search:
-                <input
-                placeholder="Student Name"
-                value={search}
-                onChange={(e)=>setSearch(e.target.value)}
-                />
-                </label>
+      <>
+      <h1>AdminDashboard</h1>
+      <div>
+          <form onSubmit={answer}>
+              <label>Search:
+              <input
+              placeholder="Student Name"
+              value={search}
+              onChange={(e)=>setSearch(e.target.value)}
+              />
+              </label>
 
-                <button type="submit"> submit </button>
-            </form>
-        </div>
-        <h3>{search}</h3>
-        </>
+              <button type="submit"> submit </button>
+          </form>
+
+          {show ?( 
+          datafound ?(<>{Object.entries(studentData).map(([key,value])=> <h5 key={key}>{key.charAt(0).toUpperCase()+key.slice(1)}:{value}</h5>)} 
+            {scores.map((e)=><h5 key={e.subject}>{e.subject} = {e.score}</h5>)}
+            <button onClick={Edit}>Edit</button>
+            {edit ?
+             (<form onSubmit={UPdate} >
+                <label>Subject:
+                  <input 
+                    value={subject}
+                    onChange={(e)=>setsubject(e.target.value)}
+                    placeholder="Name of Subject"/>
+                </label>
+                <label>score:
+                  <input 
+                    value={score}
+                    onChange={(e)=>setscore(e.target.value)}
+                    placeholder="score"/>
+                </label>
+                <button type="submit">UPDATE</button>
+              </form>)
+                  :null}
+            </>)
+            :<h2>Data Not Found</h2>)
+            :null}
+      </div>
+      </>
     )
-}
+  }
 export default AdminDashboard
